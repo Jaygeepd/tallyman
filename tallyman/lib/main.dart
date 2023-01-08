c00426adaeb189986b54c82e301b92786eaef9ba
John Mahon <49533535+Jaygeepd@users.noreply.github.com>
2023-01-08 18:08:24 +0000

    Category Card start building

--- tallyman/lib/main.dart
+++ tallyman/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './models/category_model.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,6 +8,46 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.amber[400],
+          title: const Text("Tallyman"),
+        ),
+        body: Container(
+          
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.bar_chart), label: "Stats"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: "Settings")
+          ],
+          backgroundColor: Colors.amber[400],
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // Create our dummy list of containers
+  List<Category> initCategories = []
+    ..add(Category('Social', 1))
+    ..add(Category('Fitness', 1))
+    ..add(Category('Tabletop', 1));
+
+  @override
+  Widget build(BuildContext build) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
---
+++ tallyman/lib/models/category_model.dart
@@ -0,0 +1,7 @@
+// Class to hold the categories tied to a user
+class Category {
+  final String title;
+  final int userId;
+
+  Category(this.title, this.userId);
+}
